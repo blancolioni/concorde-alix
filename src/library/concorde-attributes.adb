c0c83a2882ba0dd245405e2352b0c235fdf5582f
Fraser Wilson <fraser@thewhitelion.eu>
2021-11-11 16:00:30 +0100

    Caching attributes values (now updates 30% faster!)

--- src/library/concorde-attributes.adb
+++ src/library/concorde-attributes.adb
@@ -1,12 +1,31 @@
+with WL.String_Maps;
+
 with Accord.Attribute;
 with Accord.Attribute_Value;
 
 package body Concorde.Attributes is
 
+   type Cached_Attribute_Value is
+      record
+         Attribute_Value : Accord.Attribute_Value.Attribute_Value_Handle;
+         Add             : Integer;
+         Multiply        : Real;
+      end record;
+
+   package Attribute_Maps is
+     new WL.String_Maps (Cached_Attribute_Value);
+
+   Attribute_Cache : Attribute_Maps.Map;
+
    function Attribute_Value
      (Has_Attributes : Accord.Has_Attributes.Has_Attributes_Class;
       Name           : String)
       return Accord.Attribute_Value.Attribute_Value_Class;
+
+   function Key (Has_Attributes : Accord.Has_Attributes.Has_Attributes_Class;
+                 Attribute      : String)
+                 return String
+   is (Has_Attributes.Identifier & "--" & Attribute);
 
    ---------------------
    -- Attribute_Value --
@@ -21,8 +40,9 @@ package body Concorde.Attributes is
                     Accord.Attribute.Get_By_Tag (Name);
    begin
       pragma Assert (Attribute.Has_Element, "no such attribute: " & Name);
-      return Accord.Attribute_Value.Get_By_Attribute_Value
-        (Has_Attributes, Attribute);
+      return Value : constant Accord.Attribute_Value.Attribute_Value_Handle :=
+        Accord.Attribute_Value.Get_By_Attribute_Value
+          (Has_Attributes, Attribute);
    end Attribute_Value;
 
    --------------
@@ -48,13 +68,35 @@ package body Concorde.Attributes is
       Name           : String)
       return Integer
    is
-      Value : constant Accord.Attribute_Value.Attribute_Value_Class :=
-                Attribute_Value (Has_Attributes, Name);
+      K : constant String := Key (Has_Attributes, Name);
+      Pos : constant Attribute_Maps.Cursor :=
+              Attribute_Cache.Find (K);
    begin
-      if Value.Has_Element then
-         return Value.Add;
+      if Attribute_Maps.Has_Element (Pos) then
+         return Attribute_Cache (K).Add;
       else
-         return 0;
+         declare
+            Value : constant Accord.Attribute_Value.Attribute_Value_Class :=
+                      Attribute_Value (Has_Attributes, Name);
+         begin
+            if Value.Has_Element then
+               Attribute_Cache.Insert
+                 (K,
+                  Cached_Attribute_Value'
+                    (Attribute_Value => Value.To_Attribute_Value_Handle,
+                     Add             => Value.Add,
+                     Multiply        => Value.Multiply));
+               return Value.Add;
+            else
+               Attribute_Cache.Insert
+                 (K,
+                  Cached_Attribute_Value'
+                    (Attribute_Value => Value.To_Attribute_Value_Handle,
+                     Add             => 0,
+                     Multiply        => 1.0));
+               return 0;
+            end if;
+         end;
       end if;
    end Get;
 
@@ -66,13 +108,35 @@ package body Concorde.Attributes is
      (Has_Attributes : Accord.Has_Attributes.Has_Attributes_Class;
       Name           : String) return Real
    is
-      Value : constant Accord.Attribute_Value.Attribute_Value_Class :=
-                Attribute_Value (Has_Attributes, Name);
+      K   : constant String := Key (Has_Attributes, Name);
+      Pos : constant Attribute_Maps.Cursor :=
+              Attribute_Cache.Find (K);
    begin
-      if Value.Has_Element then
-            return Value.Multiply;
+      if Attribute_Maps.Has_Element (Pos) then
+         return Attribute_Cache (K).Multiply;
       else
-         return 1.0;
+         declare
+            Value : constant Accord.Attribute_Value.Attribute_Value_Class :=
+                      Attribute_Value (Has_Attributes, Name);
+         begin
+            if Value.Has_Element then
+               Attribute_Cache.Insert
+                 (K,
+                  Cached_Attribute_Value'
+                    (Attribute_Value => Value.To_Attribute_Value_Handle,
+                     Add             => Value.Add,
+                     Multiply        => Value.Multiply));
+               return Value.Multiply;
+            else
+               Attribute_Cache.Insert
+                 (K,
+                  Cached_Attribute_Value'
+                    (Attribute_Value => Value.To_Attribute_Value_Handle,
+                     Add             => 0,
+                     Multiply        => 1.0));
+               return 1.0;
+            end if;
+         end;
       end if;
    end Get_Multiplier;
 
@@ -99,19 +163,41 @@ package body Concorde.Attributes is
       Name           : String;
       Value          : Integer)
    is
+      K : constant String := Key (Has_Attributes, Name);
+      Pos : constant Attribute_Maps.Cursor :=
+              Attribute_Cache.Find (K);
+      Cached : constant Boolean := Attribute_Maps.Has_Element (Pos);
       Rec : constant Accord.Attribute_Value.Attribute_Value_Class :=
               Attribute_Value (Has_Attributes, Name);
    begin
+      if Cached then
+         Attribute_Cache (Pos).Add := Value;
+      elsif Rec.Has_Element then
+         Attribute_Cache.Insert
+           (K, Cached_Attribute_Value'
+              (Rec.To_Attribute_Value_Handle,
+               Value, Rec.Multiply));
+      end if;
+
       if Rec.Has_Element then
          Rec.Update_Attribute_Value
            .Set_Add (Value)
            .Done;
       elsif Value /= 0 then
-         Accord.Attribute_Value.Create
-           (Has_Attributes => Has_Attributes,
-            Attribute      => Accord.Attribute.Get_By_Tag (Name),
-            Add            => Value,
-            Multiply       => 1.0);
+         declare
+            Handle : constant Accord.Attribute_Value.Attribute_Value_Handle :=
+                       Accord.Attribute_Value.Create
+                         (Has_Attributes => Has_Attributes,
+                          Attribute      => Accord.Attribute.Get_By_Tag (Name),
+                          Add            => Value,
+                          Multiply       => 1.0);
+         begin
+            if not Cached then
+               Attribute_Cache.Insert
+                 (K, Cached_Attribute_Value'
+                    (Handle, Value, 1.0));
+            end if;
+         end;
       end if;
    end Set;
 
@@ -124,19 +210,41 @@ package body Concorde.Attributes is
       Name           : String;
       Multiplier     : Real)
    is
-      Rec : constant Accord.Attribute_Value.Attribute_Value_Class :=
+      K   : constant String := Key (Has_Attributes, Name);
+      Pos : constant Attribute_Maps.Cursor :=
+              Attribute_Cache.Find (K);
+      Cached : constant Boolean := Attribute_Maps.Has_Element (Pos);
+      Rec    : constant Accord.Attribute_Value.Attribute_Value_Class :=
               Attribute_Value (Has_Attributes, Name);
    begin
+      if Cached then
+         Attribute_Cache (Pos).Multiply := Multiplier;
+      elsif Rec.Has_Element then
+         Attribute_Cache.Insert
+           (K, Cached_Attribute_Value'
+              (Rec.To_Attribute_Value_Handle,
+               Rec.Add, Multiplier));
+      end if;
+
       if Rec.Has_Element then
          Rec.Update_Attribute_Value
            .Set_Multiply (Multiplier)
            .Done;
       elsif Multiplier /= 1.0 then
-         Accord.Attribute_Value.Create
-           (Has_Attributes => Has_Attributes,
-            Attribute      => Accord.Attribute.Get_By_Tag (Name),
-            Add            => 0,
-            Multiply       => Multiplier);
+         declare
+            Handle : constant Accord.Attribute_Value.Attribute_Value_Handle :=
+                       Accord.Attribute_Value.Create
+                         (Has_Attributes => Has_Attributes,
+                          Attribute      => Accord.Attribute.Get_By_Tag (Name),
+                          Add            => 0,
+                          Multiply       => Multiplier);
+         begin
+            if not Cached then
+               Attribute_Cache.Insert
+                 (K, Cached_Attribute_Value'
+                    (Handle, 0, Multiplier));
+            end if;
+         end;
       end if;
    end Set;
 
